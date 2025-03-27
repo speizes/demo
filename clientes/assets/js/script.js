fa1181156bd7c49497de827528a4d2264c47fe95
speizes <noreply@speisez.com>
2025-03-27 11:52:30 -0600

    Add files via upload

---
+++ clientes/assets/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        // Redirigir a la siguiente página después de 3 segundos
+        window.location.href = 'presentacion.html';
+    }, 3000);
+};
